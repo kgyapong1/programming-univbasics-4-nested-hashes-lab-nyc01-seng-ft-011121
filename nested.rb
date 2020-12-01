65f9d6a5770b6a6ef49eaec658304de7d5a39db4
Kwaku Gyapong <kwakugyapong219@gmail.com>
2020-12-01 21:12:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,6 +69,9 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  
+ 		  
+ 		  
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -82,7 +85,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto] ={:known_for => "Ruby",:languages => ["LISP, C"]}
+
 end
 
 def changing_alan
