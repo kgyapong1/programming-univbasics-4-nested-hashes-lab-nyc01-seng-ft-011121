a6658f8d151e5330a6231d301343c384afd555be
Kwaku Gyapong <kwakugyapong219@gmail.com>
2020-12-01 21:17:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,8 +126,8 @@ def adding_to_dennis
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => ["C", "Assembly"]
       }
     }
-programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
 end
